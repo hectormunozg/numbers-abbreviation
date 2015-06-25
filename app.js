bb65ee0c52981ba620b886f5deee60df156eeee6
hectormunozg <hectormunozgarcia@gmail.com>
2015-06-25 01:02:23 +0100

    declaring variables

---
+++ app.js
@@ -0,0 +1,10 @@
+// variables
+var customSelector 	 = "dataconvert";
+var kFactor 		 = "K";
+var mFactor 		 = "M";
+var decNumK 		 = 2;
+var decNumM 		 = 2;
+var separator 		 = '.';
+var realNumbers 	 = [];
+var convertedNumbers = [];
+
